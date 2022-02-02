0c5b60e5b31595fcf4d3299b24080248eac97778
vanderms <vanderms.84@gmail.com>
2022-02-01 22:53:50 -0300

    apply prettier to solution

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const share = document.querySelector('.share');
 const shareButton = document.querySelector('.share-button');
 const popover = document.querySelector('.pop-over');
 
-
 shareButton.addEventListener('click', () => {
   shareButton.classList.toggle('active');
   popover.classList.toggle('active');
